5e0348ffd0f7a67cd62555d7bf5b3cb6839eb0c1
Igor Ignatyev <themrrrudlf@gmail.com>
2018-06-16 14:35:50 +0300

    Attempt #1

--- src/main/java/com/epam/homework/task24/Task24Impl.java
+++ src/main/java/com/epam/homework/task24/Task24Impl.java
@@ -1,21 +1,17 @@
 package com.epam.homework.task24;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class Task24Impl implements Task24 {
 
     @Override
     public Map<Integer, Integer> addPolynomials(Map<Integer, Integer> first, Map<Integer, Integer> second) {
 
-        Map<Integer, Integer> result = new HashMap<>();
+        Map<Integer, Integer> result = new HashMap<>(first);
 
-        Set<Integer> coefficients = new HashSet<>(first.keySet());
-        coefficients.addAll(second.keySet());
-        for (Integer key : coefficients) {
-            result.put(key, first.get(key) + second.get(key));
+        for (Map.Entry<Integer, Integer> entry : second.entrySet()) {
+            result.merge(entry.getKey(), entry.getValue(), (val1, val2) -> val1 + val2);
         }
 
         return result;
