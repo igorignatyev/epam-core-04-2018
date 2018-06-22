5274eb7f9d26a1e47abe62ed746d51bcd5be0e61
Igor Ignatyev <themrrrudlf@gmail.com>
2018-06-22 14:57:06 +0300

    Why not

--- src/main/java/com/epam/homework/Task7.java
+++ src/main/java/com/epam/homework/Task7.java
@@ -3,6 +3,7 @@ package com.epam.homework;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Task7 {
 
@@ -11,52 +12,56 @@ public class Task7 {
      * Найти слова, состоящие только из различных символов английского алфавита.
      * Символы верхнего и нижнего регистра считать одинаковыми.
      * В случае, если слово встречается более одного раза - вывести его единожды.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
      * Строка, содержащая нимимум N слов (состоящих только из букв английского языка), разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должна быть выведена строка, содержащая найденные слова, разделенные пробелами.
      * Порядок слов должен совпадать с порядком их появления во входной последовательности.
      * В случае, если не найдено ни одного слова, удовлетворяющего условию - в поток должно быть выведено "NOT FOUND".
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 11
      * The Java programming language is a general-purpose, concurrent, class-based, object-oriented language
-     *
+     * <p>
      * Выходные данные:
      * The is a
      */
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int numberOfWords = scanner.nextInt();
-        LinkedHashSet<String> uniqueLetteredWords = new LinkedHashSet<>();
-        for (int i = 0; i < numberOfWords; i++){
+        Set<String> uniqueLetteredWords = new LinkedHashSet<>();
+
+        for (int i = 0; i < numberOfWords; i++) {
             String currentWord = scanner.next();
             if (isUniqueLatinLettered(currentWord)) {
                 uniqueLetteredWords.add(currentWord);
             }
         }
+
         if (uniqueLetteredWords.isEmpty()) {
             System.out.println("NOT FOUND");
             return;
         }
-        for (String word: uniqueLetteredWords) {
+        for (String word : uniqueLetteredWords) {
             System.out.print(word + " ");
         }
     }
 
-    private static boolean isUniqueLatinLettered(String word){
+    private static boolean isUniqueLatinLettered(String word) {
+
         HashSet<Character> charsInCurrentWord = new HashSet<>();
-        String temporaryWord = word.toLowerCase();
-        for (int i = 0; i < temporaryWord.length(); i++){
-            char currentChar = temporaryWord.charAt(i);
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.toLowerCase().charAt(i);
             charsInCurrentWord.add(currentChar);
         }
-        return charsInCurrentWord.size() == temporaryWord.length();
+        return charsInCurrentWord.size() == word.length();
+
     }
 }
